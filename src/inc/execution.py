d873f880a0e5cbc79f192c8e00ca61f962ba6fb4
Tomoki Tsuchida <ttsuchi@gmail.com>
2014-04-24 00:52:38 -0700

    Added tmp file handling

--- src/inc/execution.py
+++ src/inc/execution.py
@@ -10,7 +10,21 @@ from IPython.parallel import Client
 from inc.common import mtr
 
 from tempfile import mkdtemp
+from shutil import rmtree
 import os.path as path
+
+class TemporaryDirectory(object):
+    """Context manager for tempfile.mkdtemp() so it's usable with "with" statement.
+    http://stackoverflow.com/questions/6884991/how-to-delete-dir-created-by-python-tempfile-mkdtemp
+    """
+    TMP_DIR = '../tmp/'
+    
+    def __enter__(self):
+        self.name = mkdtemp(dir = TemporaryDirectory.TMP_DIR)
+        return self.name
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        rmtree(self.name)
 
 class Serial(object):
     """Executes the updates serially.
@@ -26,10 +40,11 @@ class IParallel(object):
         self.dview = self.client[:]
         
     def __call__(self, update_fn, X, As, designs, itr):
-        X_filename = path.join(mkdtemp(), 'X.dat')
-        Xm = memmap(X_filename, dtype = X.dtype, mode='w+', shape = X.shape)
-        Xm[:] = X[:]
-        return self.dview.map_sync(UpdateFunction(update_fn, mtr(Xm), itr) , zip(As, designs))
+        with TemporaryDirectory() as temp_dir:
+            X_filename = path.join(temp_dir, 'X.dat')
+            Xm = memmap(X_filename, dtype = X.dtype, mode='w+', shape = X.shape)
+            Xm[:] = X[:]
+            return self.dview.map_sync(UpdateFunction(update_fn, mtr(Xm), itr) , zip(As, designs))
 
 class UpdateFunction(object):
     """A picklable function object for the update function.
