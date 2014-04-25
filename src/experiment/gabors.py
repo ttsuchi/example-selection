30dd05924b7475e3d346dbf11dfa315cfec18592
Tomoki Tsuchida <ttsuchi@gmail.com>
2014-04-24 23:07:11 -0700

    Changed gabors to use two updaters

--- src/experiment/gabors.py
+++ src/experiment/gabors.py
@@ -7,15 +7,16 @@ Experiment 1: learn Gabor filter-generated samples.
 from data.generator import FromDictionaryL1
 from data.dictionary import RandomGabors
 from algorithms.selection import ALL_SELECTORS
-from algorithms.encoding import KSparse
+from algorithms.encoding import LASSO, KSparse
 from algorithms.updating import GD, SPAMS
 from inc.design import Experiment
 
 def create(name):
-    true_dictionary = RandomGabors(p = 12, K = 12*12)
-    selectors = [cls(200) for cls in ALL_SELECTORS]
-    encoder = KSparse(K = 3)
-    updater = SPAMS(encoder)
-    return Experiment(name, FromDictionaryL1(true_dictionary),
-                            selectors = selectors, encoders = [encoder], updaters = [updater])
+    K = 25
+    true_dictionary = RandomGabors(p = 12, K = K)
+    selectors = [cls(K * 10) for cls in ALL_SELECTORS]
+    encoder = LASSO(.15)
+    updaters = [GD(encoder, num_iter = 10), SPAMS(encoder, num_iter = 10)]
+    return Experiment(name, FromDictionaryL1(true_dictionary, lambdaS = 1, snr = .05),
+                            selectors = selectors, encoders = [encoder], updaters = updaters)
 
