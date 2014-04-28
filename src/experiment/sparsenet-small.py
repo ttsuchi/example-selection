675a4d7e9826befa265cd1a11af565da270a74e5
Tomoki Tsuchida <ttsuchi@gmail.com>
2014-04-28 16:24:19 -0700

    Added test sparsenet

---
+++ src/experiment/sparsenet-small.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+'''
+Learn a small set of dictionaries from the same dataset used in SPARSENET.
+
+@author: Tomoki Tsuchida <ttsuchida@ucsd.edu>
+'''
+from data.generator import FromImageDataset
+from algorithms.selection import *
+from algorithms.encoding import LASSO, KSparse
+from algorithms.updating import GD, SPAMS
+from inc.design import Experiment
+
+def create(name):
+    selectors = [cls(400) for cls in [Unif]]
+    plambda = 0.15
+    encoders = [LASSO(plambda)]
+    updater = GD(encoders[0])
+    return Experiment(name, FromImageDataset('../contrib/sparsenet/IMAGES.mat', p = 12, K = 144),
+                            selectors = selectors, encoders = encoders, updaters = [updater])
