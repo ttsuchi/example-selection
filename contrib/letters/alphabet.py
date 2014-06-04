f98b75d722b0cfcc6592b8b5b0464ba9dcf88b62
Tomoki Tsuchida <ttsuchi@gmail.com>
2014-06-04 15:00:24 -0700

    Changed the NIPS experiment to be complete / undercomplete

--- contrib/letters/alphabet.py
+++ contrib/letters/alphabet.py
@@ -8,26 +8,30 @@ from numpy import *
 from PIL import Image, ImageDraw, ImageFont
 from scipy.io import savemat
 
-def main(p):
+def main(p,k):
     letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456'
     font = ImageFont.truetype('slkscr.ttf', p)
-    L = zeros((p*p, len(letters)*2))
+    L = zeros((p*p, k))
     
     l = 0
-    sgn=1
-    for letter in letters:
+    li = 0
+    sgn = 1
+    while l < k:
+        letter = letters[li]
+        li += 1
         img = Image.new('L', (p,p))
         data = ImageDraw.Draw(img)
         data.fontmode = '1'
         data.text((0,0), letter, fill=255, font=font)
         L[:,l] = sgn*array(img).reshape((p*p,))
         l += 1
-        L[:,l] = sgn*array(img).T.reshape((p*p,))
-        l += 1
+        if k > len(letters):
+            L[:,l] = sgn*array(img).T.reshape((p*p,))
+            l += 1
         sgn *= -1
     
-    savemat("alphabet-%d.mat" % p, {'D' : L })
-    print "Created %d-by-%d letters" % (p, p)
+    savemat("letters-%d-%d.mat" % (k, p), {'D' : L })
+    print "Created %d %d-by-%d letters" % (k, p, p)
 
 if __name__ == '__main__':
     import os, sys
@@ -37,6 +41,7 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description='Creates matlab file for letters.')
     parser.add_argument('p', type=int, default=8, metavar='p', help='creates p-by-p images')
+    parser.add_argument('k', type=int, default=25, metavar='k', help='creates k dictionary elements')
 
     args = parser.parse_args()
     main(**vars(args))
